56b4575cb01a395fb203d9bca2ece0c5af98fc69
ngwa10 <todaybafutboy@gmail.com>
2025-09-13 17:43:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,83 +1,73 @@
-import random
-from telethon import TelegramClient, events
 import asyncio
+import logging
 import os
+import random
 from dotenv import load_dotenv
+from telethon import TelegramClient, events
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 load_dotenv()
-# API_ID and API_HASH
 
+# API_ID and API_HASH
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 PHONE_NUMBER = os.getenv("PHONE_NUMBER")
 
-target_group_ids_str = os.getenv("TARGET_GROUP_IDS")
+# Source group ID
+source_group_id = int(os.getenv("SOURCE_GROUP_ID", -1002944619696))
 
+# Target group IDs
+target_group_ids_str = os.getenv("TARGET_GROUP_IDS")
 try:
     target_group_ids = list(map(int, target_group_ids_str.split(',')))
-except:
+except ValueError:
     target_group_ids = []
+    logger.error("Invalid target group IDs")
 
-channel_usernames_str = os.getenv("CHANNEL_USERNAMES")
-channel_usernames = channel_usernames_str.split(',')
-
+# Create the Telegram client
 client = TelegramClient('userbot_session', API_ID, API_HASH)
 
+# Message queue
 message_queue = asyncio.Queue()
 
 async def process_message():
     while True:
-        channel_username, message = await message_queue.get()
+        message = await message_queue.get()
         message_id = message.id
         for group_id in target_group_ids:
             try:
                 await client.forward_messages(group_id, message)
-                print(f"Message ID:{message_id} forwarded to group with ID {group_id} from {channel_username}")
+                logger.info(f"Message ID:{message_id} forwarded to group with ID {group_id}")
             except Exception as e:
-                print(f"Error forwarding message to group with ID {group_id} from {channel_username}: {e}")
-        
+                logger.error(f"Error forwarding message to group with ID {group_id}: {e}")
         random_number = random.randint(int(os.getenv("MIN_TIME")), int(os.getenv("MAX_TIME")))
-        print(f"Time interval randomly chosen for {channel_username} is: {random_number}")
+        logger.info(f"Time interval randomly chosen is: {random_number}")
         await asyncio.sleep(random_number)
-
-        # Re-enqueue the message for the next round
-        await message_queue.put((channel_username, message))
+        await message_queue.put(message)
         message_queue.task_done()
 
-async def add_message_to_queue(channel, message):
-    await message_queue.put((channel, message))
-
-for channel_username in channel_usernames:
-    @client.on(events.NewMessage(chats=channel_username))
-    async def handler(event, channel=channel_username):
-        message = event.message
-        print(f"New message detected in source group with ID {channel}")
-        await add_message_to_queue(channel, message)
-
-async def get_group_ids():
-    await client.start(PHONE_NUMBER)
-    print("Group ids updated")
-    # Get all dialogs (conversations)
-    dialogs = await client.get_dialogs()
-
-    # Open a file to write the group names and IDs
-    with open('group_ids.txt', 'w', encoding="utf-8") as file:
-        for dialog in dialogs:
-            if dialog.is_group:
-                file.write(f"Group Name: {dialog.name}, Group ID: {dialog.id}\n")
+async def add_message_to_queue(message):
+    await message_queue.put(message)
 
 async def main():
-    # Start the client and log in if necessary
     await client.start(PHONE_NUMBER)
-    print("Client Created")
-    await get_group_ids()
+    logger.info("Client Created")
 
     # Start the message processing task
     asyncio.create_task(process_message())
 
+    # Set up event handler
+    @client.on(events.NewMessage(chats=source_group_id))
+    async def handler(event):
+        message = event.message
+        logger.info(f"New message detected in source group with ID {source_group_id}")
+        await add_message_to_queue(message)
+
     # Run the event handler until the script is stopped
     await client.run_until_disconnected()
-    
+
 with client:
     client.loop.run_until_complete(main())
